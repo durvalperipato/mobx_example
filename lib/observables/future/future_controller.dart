24c1f3abe7a6c0ca7c915bfd7c787eb73853a366
durvalperipato <durvalperipatoneto@gmail.com>
2022-07-23 13:33:22 -0300

    Update Options Observable Future

--- lib/observables/future/future_controller.dart
+++ lib/observables/future/future_controller.dart
@@ -6,12 +6,30 @@ class FutureController = FutureControllerBase with _$FutureController;
 
 abstract class FutureControllerBase with Store {
   @observable
+  //Opção 1
   // ObservableFuture<String> nomeFuture = Future.value('').asObservable();
-  var nomeFuture = Future.value('').asObservable();
+
+  //Opção 2
+  // var nomeFuture = Future.value('').asObservable();
+
+  // Opção 3
+  var nomeFuture = ObservableFuture.value('');
 
   @action
   Future<void> buscarNome() async {
-    nomeFuture = Future.delayed(const Duration(seconds: 2), () => 'Durval')
-        .asObservable();
+    // Opção 1
+    // nomeFuture = Future.delayed(const Duration(seconds: 2), () => 'Durval')
+    //     .asObservable();
+
+    // Opção 2
+    // var buscarNomeFuture =
+    //     Future.delayed(const Duration(seconds: 2), () => 'Durval');
+
+    // nomeFuture = buscarNomeFuture.asObservable();
+
+    // Opção 3
+    var buscaNomeFuture =
+        Future.delayed(const Duration(seconds: 2), () => 'Durval');
+    nomeFuture = ObservableFuture(buscaNomeFuture);
   }
 }
